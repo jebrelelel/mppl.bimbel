33e267d83d108af16544330f57c8c818abf5479e
jebrelelel <105858986+jebrelelel@users.noreply.github.com>
2022-05-19 19:02:23 +0700

    Add files via upload

---
+++ bimbel.mppl/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8"/>
+        <title>Laman Awal</title>
+        
+        <!-- main font -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+
+        <!-- font emoji -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+
+        <link rel="stylesheet" href="css/style.css"/>
+</head>
+</head>
+<body>
+   
+<!-- header -->
+
+<header>
+
+    <div id="menu" class="fas fa-bars"></div>
+
+    <a href="#" class="logo"></i>BIMBEL NUSANTARA</a>
+
+    <nav class="navbar">
+        <ul>
+            <li><a class="active" href="#home">HOME</a></li>
+            <li><a href="#program">PROGRAM</a></li>
+            <li><a href="login.php">LOGIN</a></li>
+            <li><a href="register.php">REGISTER</a></li>
+        </ul>
+    </nav>
+
+</header>
+
+<!-- header ends -->
+
+<!-- home  -->
+
+<section class="home" id="home">
+    
+    <h1>Mulai Belajar Bersama Kami di Bimbel Nusantara!</h1>
+    <p>Bimbel Nusantara merupakan lembaga bimbingan belajar SMA terbaik dan terpercaya di Jogja.</p>
+    <a href="register.php"><button class="btn">Register</button></a>
+
+    <div class="shape"></div>
+
+</section>
+
+<!-- home  ends -->
+
+<!-- program  -->
+
+<section class="program" id="program">
+
+<h1 class="heading">Program Yang Kami Sediakan</h1>    
+
+<div class="box-container">
+
+    <div class="box">
+        <img src="images/course1.jpg" alt="">
+        <div class="desc">
+            <div class="stars">
+            <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>            </div>
+            <p>Maksimal siswa 20/kelas</p>
+            <p>Try out bulanan untuk persiapan PTS/PAS</p>
+            <p>Evaluasi bulanan siswa</p>
+            <div class="info">
+                <h2> <i class="fa fa-clock"></i> 2 Jam </h2>
+                <h2> <i class="fa fa-calendar-alt"></i> 5 pertemuan/pekan </h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="box">
+        <img src="images/course2.jpg" alt="">
+        <div class="desc">
+            <div class="stars">
+            <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>            </div>
+            <p>Maksimal siswa 20/kelas</p>
+            <p>Try out bulanan untuk persiapan PTS/PAS</p>
+            <p>Evaluasi bulanan siswa</p>
+            <div class="info">
+                <h2> <i class="fa fa-clock"></i> 2 Jam </h2>
+                <h2> <i class="fa fa-calendar-alt"></i> 5 pertemuan/pekan </h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="box">
+        <img src="images/course3.jpg" alt="">
+        <div class="desc">
+            <div class="stars">
+                <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+            </div>
+            <p>Jaminan masuk PTN</p>
+            <p>Maksimal siswa 20/kelas</p>
+            <p>Try out bulanan untuk persiapan PTS/PAS</p>
+            <p>Evaluasi bulanan siswa</p>
+            <div class="info">
+                <h2> <i class="fa fa-clock"></i> 2 Jam </h2>
+                <h2> <i class="fa fa-calendar-alt"></i> 5 pertemuan/pekan </h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+</section>
+
+<!-- program ends -->
+
+
+<!-- footer -->
+
+<div class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="#home">Home</a>
+            <a href="#program">Program</a>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+        </div>
+
+        <div class="box">
+            <h3>Contact Info</h3>
+            <p> <i class="fa fa-map-marker-alt"></i> Gondokusuman, Yogyakarta 14211 </p>
+            <p> <i class="fa fa-envelope"></i> nusantara@bimbel.com </p>
+            <p> <i class="fa fa-phone"></i> +123-456-7890 </p>
+        </div>
+    </div>
+</div>
+
+<!-- footer ends -->
+
+</body>
+</html>
