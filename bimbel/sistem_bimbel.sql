e785382e81cb5132fc4b8a9fdc9bcce183ac8c1c
AlmightyTakkar <abimanyupalagan@gmail.com>
2022-05-20 14:31:49 +0700

    add sistem_bimbel

---
+++ bimbel/sistem_bimbel.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2022 at 09:30 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sistem_bimbel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `idAdmin` int(10) UNSIGNED NOT NULL,
+  `namaAdmin` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `murid`
+--
+
+CREATE TABLE `murid` (
+  `idMurid` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `noHP` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `waktuPendaftaran` datetime NOT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `idProgram` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `murid`
+--
+
+INSERT INTO `murid` (`idMurid`, `nama`, `noHP`, `email`, `alamat`, `waktuPendaftaran`, `password`, `idProgram`) VALUES
+(1, 'Muhammad Jibril', '085942104821', 'jebrel0912@gmail.com', 'hoeless', '2022-05-19 16:11:41', '48645045', 3),
+(2, 'Abimanyu Wahyu Palagan', '081228923690', 'abimanyupalagan@gmail.com', 'sapen', '2022-05-20 04:27:42', '50658373', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `idPembayaran` int(10) UNSIGNED NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'Belum Dibayar',
+  `waktuPembayaran` datetime NOT NULL,
+  `idMurid` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`idPembayaran`, `status`, `waktuPembayaran`, `idMurid`) VALUES
+(256001, 'Belum Dibayar', '0000-00-00 00:00:00', 1),
+(256002, 'Belum Dibayar', '0000-00-00 00:00:00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `program`
+--
+
+CREATE TABLE `program` (
+  `idProgram` int(10) UNSIGNED NOT NULL,
+  `namaProgram` varchar(20) NOT NULL,
+  `biaya` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `program`
+--
+
+INSERT INTO `program` (`idProgram`, `namaProgram`, `biaya`) VALUES
+(1, 'Kelas 10', 6000000),
+(2, 'Kelas 11', 7000000),
+(3, 'Kelas 12', 8000000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`idAdmin`);
+
+--
+-- Indexes for table `murid`
+--
+ALTER TABLE `murid`
+  ADD PRIMARY KEY (`idMurid`),
+  ADD KEY `idProgram_murid` (`idProgram`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`idPembayaran`),
+  ADD KEY `FK_pembayaran_murid` (`idMurid`);
+
+--
+-- Indexes for table `program`
+--
+ALTER TABLE `program`
+  ADD PRIMARY KEY (`idProgram`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `idAdmin` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `murid`
+--
+ALTER TABLE `murid`
+  MODIFY `idMurid` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `program`
+--
+ALTER TABLE `program`
+  MODIFY `idProgram` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `murid`
+--
+ALTER TABLE `murid`
+  ADD CONSTRAINT `FK_murid_program` FOREIGN KEY (`idProgram`) REFERENCES `program` (`idProgram`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `FK_pembayaran_murid` FOREIGN KEY (`idMurid`) REFERENCES `murid` (`idMurid`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
